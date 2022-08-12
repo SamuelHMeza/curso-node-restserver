4aa0b4f96c308816f371011bc50ab45d5f46d79a
Samuel Hamburger <sahamburger01@gmail.com>
2022-08-11 19:25:47 -0500

    Usuarios CRUD listo - 11-08-2022

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,39 +1,77 @@
-const { response } = require("express");
+const { response, reuqest } = require("express");
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
 
+const usuariosGet = async (req, res = response) => {
 
-const usuariosGet = (req, res = response) => {
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
 
-    const { q, nombre = 'no name', id } = req.query;
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(desde)
+            .limit(limite)
+
+    ]);
 
     res.json({
-        msg: 'get API -- controlador',
-        q,
-        nombre,
-        id
+        total,
+        usuarios
     });
 }
 
-const usuariosPut = (req, res) => {
+const usuariosPut = async (req, res) => {
 
-    const id = req.params.id;
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
+
+
+    console.log("u", id);
+    if (password) {
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+    res.json(usuario);
+}
+
+const usuariosPost = async (req, res) => {
+
+
+
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol });
+
+    //verificar si el correo existe
+
+
+    // Encriptar la password
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    //Guardar en BD
+    await usuario.save();
 
     res.json({
-        msg: 'put API -- controlador', id
+        usuario
     });
 }
 
-const usuariosPost = (req, res) => {
-    const { nombre, edad } = req.body;
-    res.json({
-        msg: 'post API -- controlador', nombre, edad
-    });
-} 
+const usuariosDelete = async (req, res) => {
 
-const usuariosDelete = (req, res) => {
-    res.json({
-        msg: 'delete API -- controlador'
-    });
+    const { id } = req.params; 
+
+    //Fisicamente lo borramos
+    //const usuario = await Usuario.findByIdAndDelete( id );  
+    
+    const usuario = await Usuario.findByIdAndUpdate( id, {estado: false});
+
+    res.json( usuario );
 }
 
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,14 +1,43 @@
-const { Router } =  require ('express');
-const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete } = require('../controllers/usuarios');
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+
+const { usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete } = require('../controllers/usuarios');
+
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
+
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
-router.get('/', usuariosGet )
 
-router.put('/:id', usuariosPut )
+router.get('/', usuariosGet);
 
-router.post('/', usuariosPost )
+router.put('/:id', [
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    check('rol').custom(esRoleValido),
+    validarCampos
+],usuariosPut );
 
-router.delete('/', usuariosDelete )
+router.post('/', [
+    check('nombre', 'Nombre obligatorio').not().isEmpty(),
+    check('password', 'password mas de 6 letras').isLength({ min: 6 }),
+    check('correo', 'Correo no valido').isEmail(),
+    check('correo').custom(emailExiste),
+    //check('rol', 'no es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(esRoleValido),
+
+    validarCampos
+], usuariosPost);
+
+router.delete('/:id', [
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    validarCampos
+] , usuariosDelete);
 
 module.exports = router;
